7b8c451f0149d972510bb2f6fd556a6547e19143
wuzhenxi <wuzx@hustcad.com>
2019-12-02 21:00:38 +0800

    【type】:修改 【author】:wzx 【description】:es修改

--- springboot-elasticsearch/src/main/java/com/gf/entity/Article.java
+++ springboot-elasticsearch/src/main/java/com/gf/entity/Article.java
@@ -2,7 +2,9 @@ package com.gf.entity;
 
 
 import io.searchbox.annotations.JestId;
+import lombok.Data;
 
+@Data
 public class Article {
 
     @JestId
@@ -10,53 +12,5 @@ public class Article {
     private String author;
     private String title;
     private String content;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder( "{\"Article\":{" );
-        sb.append( "\"id\":" )
-                .append( id );
-        sb.append( ",\"author\":\"" )
-                .append( author ).append( '\"' );
-        sb.append( ",\"title\":\"" )
-                .append( title ).append( '\"' );
-        sb.append( ",\"content\":\"" )
-                .append( content ).append( '\"' );
-        sb.append( "}}" );
-        return sb.toString();
-    }
-
 
 }
--- springboot-elasticsearch/src/test/java/com/gf/SpringbootElasticsearchApplicationTests.java
+++ springboot-elasticsearch/src/test/java/com/gf/SpringbootElasticsearchApplicationTests.java
@@ -1,20 +1,13 @@
 package com.gf;
 
-import com.gf.entity.Article;
 import com.gf.entity.Book;
 import com.gf.repository.BookRepository;
-import io.searchbox.client.JestClient;
-import io.searchbox.core.Index;
-import io.searchbox.core.Search;
-import io.searchbox.core.SearchResult;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.IOException;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
